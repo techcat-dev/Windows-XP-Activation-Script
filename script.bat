4f7c919b92ee2285809a61fab542dcf8485dc31b
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-09-24 20:40:13 -0400

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+echo Windows XP Activator By TechCat-Dev
+echo https://github.com/techcat-dev
+echo:
+echo Press Enter to Permanently Activate Your Windows XP Machine.
+echo:
+pause
+cls
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents" /v OOBETimer /t REG_BINARY /d FFD571D68B6A8D6FD53393FD /f
+echo HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents [1] >> perms.txt
+regini perms.txt
+del perms.txt
+cls
+echo Activation Complete! Enjoy!
+pause
+
