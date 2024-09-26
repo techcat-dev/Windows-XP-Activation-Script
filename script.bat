920bd4b7334ece1db10b744963cf401be7ee2073
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-09-26 17:21:47 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -8,7 +8,7 @@ echo:
 pause
 cls
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents" /v OOBETimer /t REG_BINARY /d FFD571D68B6A8D6FD53393FD /f
-echo HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents [1] >> perms.txt
+echo HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents [0] >> perms.txt
 regini perms.txt
 del perms.txt
 cls
